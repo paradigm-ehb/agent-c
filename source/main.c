5a68a07436b53251a132d19752e416ac131e6f54
Abdellah El Morabit <nsrddyn@gmail.com>
2025-10-26 21:25:15 +0100

    [bugfix] main function definition

--- source/main.c
+++ source/main.c
@@ -8,37 +8,11 @@ void get_ram();
 void get_disk();
 void get_device();
 
-void get_c_version()
-{
-
-#if __STDC_VERSION__
-  printf("C standard version: %ld\n", __STDC_VERSION__);
-#else
-  printf("C standard not defined\n");
-#endif
-
-
-}
-
-#ifdef __APPLE__
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
   printf("%d", u_cpu->temperature);
 
   return 0;
 }
 
-#endif
-
-#ifdef __gnu_linux
-
-int main(int argc, char** argv) {
-
-
-  return 0;
-
-}
-
-#endif
 
