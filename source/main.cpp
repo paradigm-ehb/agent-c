b3c249dc36ce83a594686c5c88fb4396b8091be2
nasr <nsrddyn@gmail.com>
2025-12-11 00:23:38 +0100

    refactor: merged from git.nsrddyn.com/nasr/resources-tool

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+
 /*
  * 
  * Author: nasr
@@ -5,43 +6,22 @@
  *
  */
 
+#include <sys/sysinfo.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <string.h>
-#include <sys/sysinfo.h>
+#include <math.h>
 
 #define MAXC 1024
 #define MAXC_CHAR 256
 #define CONVERT_BYTES_TO_GIGABYTES 107374182   
 #define D 1073741824
 
-
-void cpu_name();
-void cpu_threads();
-void cpu_temperature();
-void cpu_frequency();
-
-void get_total();
-void get_usage();
-
-void av_size();
-void ram_info();
-
-void mem_size();
-void mem_av_size();
-
-void device_os_version();
-void device_model();
-void device_up_time();
-void device_model();
-
+#define forever for(;;)
 
 typedef struct {
 
@@ -50,17 +30,17 @@ typedef struct {
 	int temperature;
 	int threads;
 
-} cpu_s;
+} Cpu;
 
 typedef struct {
-	long int total;
-	long int available;
-} ram_s;
+	double total;
+	double available;
+} Ram;
 
 typedef struct {
 	long size;
 	char *name;
-} disk_s;
+} Disk;
 
 typedef struct { 
 
@@ -68,118 +48,100 @@ typedef struct {
 	char *hostname;
 	char *os_version;
 	int uptime;
+	uint16_t procs;
 
-} device_s;
+} Device;
 
+void cpu_data(Cpu *cpu);
+void memory_data(Ram *ram);
+void device_data(Device *device);
 
-cpu_s *u_cpu;
-ram_s *u_ram;
-disk_s *u_disk;
-device_s *u_device;
+int 
+main() {
 
-void init() {
+	// TODO: get disk information
 
-	u_cpu = (cpu_s*)malloc( sizeof(cpu_s) );
-	u_ram = (ram_s*)malloc(sizeof(ram_s));
-	u_disk =(disk_s*) malloc(sizeof(disk_s));
-	u_device = (device_s*)malloc(sizeof(device_s));
+	Cpu		*cpu		= (Cpu*)malloc( sizeof(Cpu) );
+	Ram		*ram		= (Ram*)malloc(sizeof(Ram));
+	Device	*device		= (Device*)malloc(sizeof(Device));
 
-	cpu_name();
-	cpu_frequency();
-	cpu_threads();
-	cpu_temperature();
+	// disk	= (Disk*)malloc(sizeof(Disk));
 
-	mem_size();
-	mem_av_size();
+	cpu_data(cpu);
+	memory_data(ram);
+	device_data(device);
 
-	get_total();
-	get_usage();
+	printf("gathering system resources...");
+	printf("\ntemperature: %d\nfrequency: %s\nname: %s\nthreads: %d\n", cpu->temperature, cpu->frequency, cpu->name, cpu->threads);
+	printf("total: %f\navailable: %f\n", ram->total, ram->available);
+	printf("device name:%s\ndevice hostname:%s\nos version: %s\nuptime: %d\nprocs: %d\n", device->name, device->hostname, device->os_version, device->uptime, device->procs);
 
-	device_os_version();
-	device_model();
-	device_up_time();
-	device_model();
-
-}
-
-void end() {
-
-	free(u_cpu);
-	free(u_ram);
-	free(u_disk);
-	free(u_device);
-
-}
-
-int main() {
-
-	init();
-
-	printf("temperature: %d\nfrequency: %s\nname: %s\nthreads: %d\n", u_cpu->temperature, u_cpu->frequency, u_cpu->name, u_cpu->threads);
-	printf("total: %ld\navailable: %ld\n", u_ram->total, u_ram->available);
-	printf("device name:%s\ndevice hostname:%s\nos version: %s\nuptime: %d\n", u_device->name, u_device->hostname, u_device->os_version, u_device->uptime);
-
-	// free program
-	end();
-
+	free(cpu);
+	free(ram);
+	// free(disk);
+	free(device);
 
 	return 0;
 }
 
 
 
-void
-cpu_name() {
 
-	int *buffer_size = (int*)malloc(sizeof(int));
-	char *cpu_name = (char*)malloc(sizeof(char));
+
+void
+cpu_data(Cpu *cpu) {
+
+	cpu->name	= (char*)malloc(sizeof(char));
+	char *buffer = (char*)malloc(sizeof(int));
 
 	FILE *fp = fopen("/proc/cpuinfo", "r");
 	if (!fp) perror("can't open /proc/cpuinfo");
 
-	char line[sizeof(cpu_name)];
+	char line[sizeof(cpu->name)];
 	while (fgets(line, sizeof(line), fp)) {
-		if (strncmp(line, "model name", 10) == 0) {
+		if (strncmp(line, "model",  sizeof(cpu->name)) == 0) {
 			char *colon = strchr(line, ':');
 			if (colon) {
-				int err = fclose(fp);
-				if (err != 0)
-					perror("error closing /proc/cpuinfo");
+					cpu->name[strcspn(cpu->name, "\n")] = 0;
+					snprintf(cpu->name, MAXC_CHAR, "%s", colon);
 			}
+
+
+			if (strstr(buffer, "cpu MHz") != NULL) {
+				colon = NULL;
+				colon = strchr(buffer, ':');
+				if (colon) {
+					buffer[strcspn(buffer, "\n")] = 0;
+					snprintf(buffer, MAXC_CHAR, "%s", colon);
+					cpu->frequency = buffer;
+				}
+			}
+			int err = fclose(fp);
+			if (err != 0) perror("error closing /proc/cpuinfo");
 		}
 	}
 
-	u_cpu->name = cpu_name;
+	fclose(fp);
+	fp = NULL;
 
+#define delay 1
+
+	fp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+	if (!fp)
+		printf("error reading /proc/cpuinfo");
+
+	while (fgets(buffer, sizeof(buffer), fp)) {
+		int a = atoi(buffer);
+		a /= 1000;
+		cpu->temperature = a;
+		fflush(stdout);
+	}
+	fclose(fp);
 }
 
-void
-cpu_frequency() {
-	char* buffer = (char*)malloc(MAXC_CHAR);
-
-	FILE *fp = fopen("/proc/cpuinfo", "r");
-	if (!fp) {
-		printf("can't open /proc/cpuinfo");
-		return;
-	}
-
-	while (fgets(buffer, MAXC_CHAR, fp)) {
-		if (strstr(buffer, "cpu MHz") != NULL) {
-			char *colon = strchr(buffer, ':');
-			if (colon) {
-				buffer[strcspn(buffer, "\n")] = 0;
-				snprintf(buffer, MAXC_CHAR, "%s", colon);
-			}
-		}
-
-		fclose(fp);
-		u_cpu->frequency = buffer;
-	}
-	return;
-}
 
 void
-mem_size() {
+memory_data(Ram *ram) {
 
 	struct sysinfo info;
 
@@ -189,92 +151,37 @@ mem_size() {
 	}
 
 	long total_ram = info.totalram * info.mem_unit; 
-	u_ram->total = total_ram;
-
-}
-
-
-void
-mem_av_size() {
-	struct sysinfo info;
 
 	if (sysinfo(&info) != 0) {
 		perror("sysinfo");
 	}
 
-	long total_ram = info.totalram * info.mem_unit;
 	long free_ram = info.freeram * info.mem_unit;
 
-	u_ram->available = free_ram;
+	ram->available	= free_ram / pow(10, 8);
+	ram->total		= total_ram /  pow(10, 8);
+
+}
+
+void
+disk_data(Disk *disk) 
+{
+
+	// TODO: return disk data
+
 }
 
 
 void
-cpu_temperature() {
-
-	int delay = 0;
-	while (1) {
-		sleep(delay);
-		FILE *pf = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
-		if (!pf)
-			printf("error reading /proc/cpuinfo");
-
-		char buffer[MAXC];
-		while (fgets(buffer, sizeof(buffer), pf)) {
-			int a = atoi(buffer);
-			a /= 1000;
-			printf("cpu temperature: %dC\n", a);
-			fflush(stdout);
-		}
-		fclose(pf);
-	}
-}
-
-
-void
-device_up_time() {
+device_data(Device *device) {
 	struct sysinfo info;
 	if (sysinfo(&info) == -1)
 		perror("sysinfo");
 
-	u_device->uptime = info.uptime;
-}
-
-
-void
-device_model() {
-
-	// TODO(nasr): 
-
-	return;
-
-}
-
-void
-cpu_threads() {
-
-	// TODO(nasr): 
-	return;
-}
-
-void
-get_total() {
-
-
-	// TODO(nasr): 
-}
-
-void
-get_usage() {
-
-
-	// TODO(nasr): 
-}
-
-void
-device_os_version() {
-
-	// TODO(nasr): 
-
+	device->uptime = info.uptime;
+	// device->hostname = info.procs;
+	device->procs = info.procs;	
+	// TODO: get device model
+	// TODO: get distribution and os version 
 }
 
