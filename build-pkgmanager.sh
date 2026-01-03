7289e957c313613c2a3a7ce6391278a397766360
nasr <nsrddyn@gmail.com>
2026-01-03 14:35:30 +0100

    featuer: package manager build script

---
+++ build-pkgmanager.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -eu
+
+CXX=c++
+SRC=dnf_handler.cpp
+OUT=build/pkgmanager
+
+CXXFLAGS="
+-std=c++20
+-Wall -Wextra -Wpedantic
+-Wshadow -Wconversion
+-Wundef
+-Wpointer-arith -Wcast-align -Wcast-qual
+-Wformat=2 -Wformat-security
+-Wnull-dereference
+-Wmisleading-indentation
+-Wunused -Wuninitialized
+-fexceptions
+-fno-rtti
+-ldnf5
+-lfmt
+"
+
+mkdir -p build
+
+echo "Compiling..."
+$CXX $CXXFLAGS "$SRC" -o "$OUT"
+
+echo "Running..."
+"$OUT"
