404f56e54c5e3373f529f0f5b59d6fed2fa241d8
nasr <nsrddyn@gmail.com>
2026-01-08 23:44:29 +0100

    feature: main entry point

---
+++ resources_main.c
@@ -0,0 +1,45 @@
+#include "base.h"
+#include "resources.h"
+#include "stdio.h"
+#include "arena.h"
+#include "arena.c"
+#include "resources.c"
+#include <assert.h>
+
+
+int main()
+{
+
+  mem_arena *arena = arena_create(MiB(1));
+
+  Device *device =  device_create(arena);
+  process_list_collect(&device->processes, arena);
+
+  for (size_t proc_idx;
+    proc_idx < device->processes.count;
+    ++proc_idx)
+    {
+
+      Process *proc = (Process*)arena_push(arena,
+        sizeof(Process),
+        1);
+
+      i32 error = process_read(device->processes
+      
+      .items[proc_idx]
+      .pid,
+      proc);
+
+
+      if (error != OK)
+      {
+
+        assert(0);
+      }
+
+    printf("after %d\n", proc->state);
+    }
+
+
+  return 0;
+}
